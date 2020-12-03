c91eeb5d2d95293817c14c6295d9687034b0c2d8
Abdo Hanna <abdo.hanna@student.manchester.ac.uk>
2020-12-03 19:04:46 +0000

    Replaced the Node class with the Tree class

--- MKAgent/Node.java
+++ MKAgent/Node.java
@@ -1,25 +1,25 @@
-package MKAgent;
-
-import java.util.ArrayList;
-
-
-public class Node {
-   public ArrayList<Node> children;
-   public Node parent;
-   public Board board;
-
-   public int depth;
-   public int heuristicValue;
-
-   // heuristicValue value to be dded in the constructor
-   public Node(Node nodeParent, Board gameState, int theDepth) {
-      board = gameState;
-      children = new ArrayList();
-      depth = theDepth;
-      parent = nodeParent;
-      //heuristicScore = call: getScore();
-   }
-   public void addChild(Node node) {
-      children.add(node);
-   }
-}
+// package MKAgent;
+//
+// import java.util.ArrayList;
+//
+//
+// public class Node {
+//    public ArrayList<Node> children;
+//    public Node parent;
+//    public Board board;
+//
+//    public int depth;
+//    public int heuristicValue;
+//
+//    // heuristicValue value to be dded in the constructor
+//    public Node(Node nodeParent, Board gameState, int theDepth) {
+//       board = gameState;
+//       children = new ArrayList();
+//       depth = theDepth;
+//       parent = nodeParent;
+//       //heuristicScore = call: getScore();
+//    }
+//    public void addChild(Node node) {
+//       children.add(node);
+//    }
+// }
